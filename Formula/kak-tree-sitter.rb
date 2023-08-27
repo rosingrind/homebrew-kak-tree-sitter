dde4b7df86647058817764dc4bd125aa0c5dcc46
Adam Barton <108860307+rosingrind@users.noreply.github.com>
2023-08-27 15:31:41 +0000

    kak-tree-sitter: add 0.4.0 bottle.

--- Formula/kak-tree-sitter.rb
+++ Formula/kak-tree-sitter.rb
@@ -5,6 +5,12 @@ class KakTreeSitter < Formula
   sha256 "26a3418935dae0b398ffcb485e37acaa274e744b224611589acfbebf4b46fdde"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://github.com/rosingrind/homebrew-kak-tree-sitter/releases/download/kak-tree-sitter-0.4.0"
+    sha256 cellar: :any_skip_relocation, ventura:      "b41d15b6e9c160849310358b16d57a88a08bd5db7c5a1e9846b19ecbf5f6a36a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a6de97d2b0ef5a56fd015bbb2cc6dcbe97e86661da0f6d28e4d8136562916eca"
+  end
+
   depends_on "rust" => :build
 
   def install
